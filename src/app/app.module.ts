a9480994b5f61fb820502b14a5a8bb251dc79737
zsnuffer <107090571+zsnuffer@users.noreply.github.com>
2022-09-07 15:32:06 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UploadsModule } from '@progress/kendo-angular-upload';
 
 import { GanttModule } from '@progress/kendo-angular-gantt';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { DatePickerModule } from '@progress/kendo-angular-dateinputs';
 import { AppComponent } from './app.component';
-
+import { TaskListComponent } from './TaskList/TaskList.component';
+import { WorkFlowDetailComponent } from './WorkflowDetail/WorkflowDetail.component';
 @NgModule({
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        ReactiveFormsModule,
-        GanttModule
-    ],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    GanttModule,
+    GridModule,
+    DropDownsModule,
+    DatePickerModule,
+    UploadsModule,
+  ],
+  declarations: [AppComponent, TaskListComponent, WorkFlowDetailComponent],
+  bootstrap: [WorkFlowDetailComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
